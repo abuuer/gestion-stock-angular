1ff35ebcb23217f9b9cb09feae817dec3d1a8dca
bart-khalid <bartaouch14@gmail.com>
2020-03-29 18:19:50 +0100

    adding attributes

--- src/app/controller/model/administration/entite-administrative.model.ts
+++ src/app/controller/model/administration/entite-administrative.model.ts
@@ -1,6 +1,14 @@
+import {Personnel} from './personnel.model';
+import {Magasin} from '../magasin/magasin.model';
+import {ExpressionBesion} from './expression-besion.model';
+
 export class EntiteAdministrative {
-  public reference : String ;
-  public nom: String ;
+  public reference: string ;
+  public nom: string ;
+  public employes: Array<Personnel> ;
+  public magasins: Array<Magasin> ;
+  public expressionBesoins: Array<ExpressionBesion> ;
+  public chef: Personnel ;
 
   constructor() {
   }
--- src/app/controller/model/magasin/magasin.model.ts
+++ src/app/controller/model/magasin/magasin.model.ts
@@ -1,10 +1,17 @@
+import {Personnel} from '../administration/personnel.model';
+import {Stock} from './stock.model';
+import {EntiteAdministrative} from '../administration/entite-administrative.model';
+
 export class Magasin {
-  public reference : String ;
-  public nom : String ;
-  public nbrMAxProduit : number ;
-  public nbrProduit : number ;
-  public nbrMaxEmploye : number ;
-  public nbremploye : number ;
+  public reference: string ;
+  public nom: string ;
+  public nbrMAxProduit: number ;
+  public nbrProduit: number ;
+  public nbrMaxEmploye: number ;
+  public nbremploye: number ;
+  public employes = new Array<Personnel>();
+  public stocks = new Array<Stock>();
+  public entiteAdministrative: EntiteAdministrative;
 
   constructor() {
   }
